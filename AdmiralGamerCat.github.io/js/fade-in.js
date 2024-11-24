62bbfe0c297a63d68f2608418bdf0987100dabeb
Hidde Aalders <hidde.j.p.aalders@gmail.com>
2024-11-24 21:41:24 +0100

    added pc-builds and some file structure changes

--- AdmiralGamerCat.github.io/js/fade-in.js
+++ AdmiralGamerCat.github.io/js/fade-in.js
@@ -36,39 +36,3 @@ document.querySelectorAll('[data-popup-target]').forEach(trigger => {
         trigger.addEventListener("click", openPopup);
     }
 });
-
-// theme switching
-const checkbox = document.getElementById('theme-toggle');
-
-// Check for saved theme in localStorage
-const currentTheme = localStorage.getItem('theme');
-if (currentTheme) {
-  document.body.classList.add(currentTheme);
-  if (currentTheme === 'light-mode') {
-    checkbox.checked = true;
-  }
-}
-
-checkbox.addEventListener('change', () => {
-  if (checkbox.checked) {
-    document.body.classList.add('light-mode');
-    document.body.classList.remove('dark-mode');
-    localStorage.setItem('theme', 'light-mode');
-  } else {
-    document.body.classList.add('dark-mode');
-    document.body.classList.remove('light-mode');
-    localStorage.setItem('theme', 'dark-mode');
-  }
-});
-
-function openPopup(imageSrc) {
-  var popup = document.getElementById("image-popup");
-  var popupImg = document.getElementById("popup-img");
-  popup.style.display = "flex"; // Display the popup
-  popupImg.src = imageSrc; // Set the image source
-}
-
-function closePopup() {
-  var popup = document.getElementById("image-popup");
-  popup.style.display = "none"; // Hide the popup
-}
