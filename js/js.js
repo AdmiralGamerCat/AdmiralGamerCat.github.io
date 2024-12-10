2c7db3a9d9c0b9c3ece78f819fdd3c879f7907ba
AdmiralGamerCat <hidde.j.p.aalders@gmail.com>
2024-12-10 01:41:27 +0100

    improved mobile supprt and added wip easter egg

--- js/js.js
+++ js/js.js
@@ -1,7 +1,20 @@
+// smoothly slide in content on page load
 document.addEventListener("DOMContentLoaded", () => {
     const boxes = document.querySelectorAll(".loaded");
     boxes.forEach((box, index) => {
         box.style.animationDelay = `${index * 0.2}s`; // Staggered delay for each box
         box.classList.add("animated"); // Add the class to trigger animation
     });
+});
+
+// for rainbow background
+const wipImg = document.querySelector('.wip-img');
+const body = document.body;
+
+wipImg.addEventListener('mouseenter', () => {
+    body.classList.add('rainbow-background');
+});
+
+wipImg.addEventListener('mouseleave', () => {
+    body.classList.remove('rainbow-background');
 });
